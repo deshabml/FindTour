3f5bfda159aeb49368548ab9ecf6dd36b8a3a062
Михаил Дмитриев <korncl48@gmail.com>
2024-06-10 17:15:01 +0300

    T-OO1 Add Module Constant

---
+++ Constants/Constants/Constants.h
@@ -0,0 +1,18 @@
+//
+//  Constants.h
+//  Constants
+//
+//  Created by macbook on 10.06.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Constants.
+FOUNDATION_EXPORT double ConstantsVersionNumber;
+
+//! Project version string for Constants.
+FOUNDATION_EXPORT const unsigned char ConstantsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Constants/PublicHeader.h>
+
+
---
+++ Constants/Constants/Module/UIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  UIConstants.swift
+//  Constants
+//
+//  Created by macbook on 10.06.2024.
+//
+
+import Foundation
+
+public struct UIConstants { }
+
+// MARK: - Extension
+
+public extension UIConstants {
+  static let testConstant: CGFloat = 44
+}
