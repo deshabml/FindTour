ed43baecf0b5368b3b116ee8a959a6f6601f0baf
deshabml <deshab@yandex.ru>
2024-06-10 16:04:38 +0300

    Initial Commit

---
+++ FindTour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FindTour
+//
+//  Created by Лаборатория on 10.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FindTour/FindTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTourApp.swift
+//  FindTour
+//
+//  Created by Лаборатория on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FindTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
